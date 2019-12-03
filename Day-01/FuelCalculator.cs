f185ce0d1336214832e47ca83c44e5ae0c4a5a64
Duncan Roosma <github@roosma.me>
2019-12-03 15:06:23 +0100

    Add part two of day one

--- Day-01/FuelCalculator.cs
+++ Day-01/FuelCalculator.cs
@@ -6,5 +6,20 @@ namespace Day_01
     {
         public static decimal ByMass(decimal mass)
             => Math.Floor(mass / 3) - 2;
+
+        public static decimal ByMassWithFuel(decimal mass)
+        {
+            var fuelRequirement = ByMass(mass);
+            var sum = fuelRequirement;
+            do
+            {
+                fuelRequirement = ByMass(fuelRequirement);
+                if (fuelRequirement > 0)
+                    sum += fuelRequirement;
+            }
+            while (fuelRequirement > 0);
+
+            return sum;
+        }
     }
 }
---
+++ Day-01/PartTwo.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using System;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Day_01
+{
+    public class PartTwo
+    {
+        [Theory]
+        [InlineData(1969, 966)]
+        [InlineData(100756, 50346)]
+        public void FromExample(decimal mass, decimal fuelRequirement)
+        {
+            var result = FuelCalculator.ByMassWithFuel(mass);
+            result.Should().Be(fuelRequirement);
+        }
+
+        [Fact]
+        public void FromInput()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "input-part-one.txt";
+
+            var input = Utilities.EmbeddedResource.Get(resourceName, assembly);
+            var fuelRequirements = input.Split(Environment.NewLine)
+                .AsParallel()
+                .Select(value => decimal.Parse(value))
+                .Select(mass => FuelCalculator.ByMassWithFuel(mass))
+                .Sum();
+
+            fuelRequirements.Should().Be(4876469);
+        }
+    }
+}
