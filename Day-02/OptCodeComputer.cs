62a44e819ebced49c7da7195d6bb329bed5af208
Duncan Roosma <github@roosma.me>
2019-12-03 21:38:12 +0100

    Answer Day 2 part 2

--- Day-02/OptCodeComputer.cs
+++ Day-02/OptCodeComputer.cs
@@ -13,7 +13,7 @@ namespace Day_02
             var index = 0;
             while ((index = Compute(ref input, index)) > 0) ;
 
-            int Compute(ref int[] source, int index)
+            static int Compute(ref int[] source, int index)
             {
                 var cache = source.Skip(index)
                                  .Take(4)
@@ -21,27 +21,21 @@ namespace Day_02
 
                 var optCode = cache[0];
 
+                if (optCode == exit) return -1;
+
+                var noun = cache[1];
+                var verb = cache[2];
+                var destination = cache[3];
+
                 if (optCode == add)
                 {
-                    var first = cache[1];
-                    var second = cache[2];
-                    var destination = cache[3];
-
-                    var result = source[first] + source[second];
+                    var result = source[noun] + source[verb];
                     source[destination] = result;
                 }
                 else if (optCode == multiply)
                 {
-                    var first = cache[1];
-                    var second = cache[2];
-                    var destination = cache[3];
-
-                    var result = source[first] * source[second];
+                    var result = source[noun] * source[verb];
                     source[destination] = result;
-                }
-                else if (optCode == exit)
-                {
-                    return -1;
                 }
 
                 return index + 4;
---
+++ Day-02/PartTwo.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Day_02
+{
+    public class PartTwo
+    {
+        [Fact]
+        public void FromInput()
+        {
+            const int expectedOutput = 19690720;
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "input.txt";
+
+            var input = Utilities.EmbeddedResource.Get(resourceName, assembly);
+
+            var optCodes = input.Split(",")
+                                .Select(int.Parse)
+                                .ToArray();
+
+            var nouns = Enumerable.Range(0, 99);
+            var verbs = Enumerable.Range(0, 99);
+
+            Parallel.ForEach(nouns, noun =>
+            {
+                Parallel.ForEach(verbs, verb =>
+                {
+                    var cache = new int[optCodes.Length];
+                    System.Array.Copy(optCodes, cache, optCodes.Length);
+                    cache[1] = noun;
+                    cache[2] = verb;
+
+                    OptCodeComputer.Output(ref cache);
+
+                    if (cache[0] == expectedOutput)
+                        System.Console.WriteLine($"waaaa noun {noun} - verb {verb} result = {100 * noun + verb}");
+                });
+            });
+        }
+    }
+}
