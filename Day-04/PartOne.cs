bb4e5ba5edf5e0e171b42c528eaf2d52aa2479fc
Duncan Roosma <github@roosma.me>
2019-12-05 10:50:46 +0100

    From example works, but this is not correct answer.

--- Day-04/PartOne.cs
+++ Day-04/PartOne.cs
@@ -43,4 +43,67 @@ namespace Day_04
             options.Count().Should().Be(1653);
         }
     }
+
+    public class PartTwo
+    {
+        [Theory]
+        [InlineData("112233", true)]
+        [InlineData("123444", false)]
+        [InlineData("111122", true)]
+        public void FromExample(string input, bool expectedResult)
+        {
+            var result = IsValidPassCode(input);
+            result.Should().Be(expectedResult);
+        }
+
+        private static bool IsValidPassCode(string code)
+        {
+            var groups = code.GroupBy(n => n)
+                             .Where(n => n.Count() == 2)
+                             .Select(n => n.Key);
+
+            if (!groups.Any())
+                return false;
+            
+            var largerGroups = code.GroupBy(n => n)
+                                   .Where(n => n.Count() > 2)
+                                   .Select(n => n.Key);
+
+            for (int i = 1; i < code.Length; i++)
+            {
+                var current = code[i];
+                var previous = code[i - 1];
+                //var next = code[i + 1];
+
+                if (current < previous)
+                    return false;
+
+                if(current == previous)
+                {
+                    var range = Enumerable.Range(i, code.Length - i);
+                    if (range.Count() > 1)
+                    {
+                        var gelijkOfGroter = range.Select(i => code[i])
+                                                  .All(c => c >= current);
+                        if (!gelijkOfGroter)
+                            return false;
+                    }
+                    else if (!groups.Contains(current))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        [Fact]
+        public void FromInput()
+        {
+            const int min = 206938;
+            const int max = 679128;
+
+            var range = Enumerable.Range(min, max - min).Select(o => o.ToString());
+            var result = range.Where(IsValidPassCode).Count();
+        }
+    }
 }
